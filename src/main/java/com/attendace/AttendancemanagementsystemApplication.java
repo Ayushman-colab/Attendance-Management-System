7418feaf461b022f417392494ee68593b1fba242
Ayushman-colab <ayushmanmishra607@gmail.com>
2025-11-08 10:55:10 +0530

    first commit

--- src/main/java/com/attendace/AttendancemanagementsystemApplication.java
+++ src/main/java/com/attendace/AttendancemanagementsystemApplication.java
@@ -1,4 +1,4 @@
-package com.spring.Attendancemanagementsystem;
+package com.attendace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
